0a9e3a871ffc4c42889fb8d8a345e7b632a02897
Carl <cwhk12a@web.de>
2021-03-20 18:58:54 +0100

    Nun mit allen Changes

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 'use strict';
+/**
+ * Author:    Carl
+ * Created:   20.03.2021
+ * Description: Main Server-file for the BierHier NodeJs Platform. Using express and ejs.
+ **/
 
+
+// Modules
 var express = require('express');
+var path = require('path');
 var routes = require('./routes/index.js');
-var port = process.env.PORT || 3000;
 
+// App setup
+var port = process.env.PORT || 3000;
 var app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
 app.set('view engine', 'ejs');
+
+// Use public folder for static content
+app.use('/public', express.static(process.cwd() + '/public'));
+
 
 routes(app);
 
+app.get('/',function (req, res) {
+    res.render('pages/home')
+});
+
+// start server
 app.listen(port, function() {
     console.log('Server listening on port ' + port + '...');
 });
